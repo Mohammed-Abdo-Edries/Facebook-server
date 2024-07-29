5cc0bb4b148a80d025ca625d0dc546e2eee31728
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2024-07-29 07:47:56 -0700

    almost finished

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require ("express");
 const mongoose = require ("mongoose");
-const dotenv = require ("dotenv");
 const helmet = require ("helmet");
 const morgan = require ("morgan");
-const userRouter = require ("./users");
-dotenv.config();
+require ("dotenv").config();
+const userRouter = require ("./routes/users");
+const authRoute = require ("./routes/auth");
+const postRoute = require ("./routes/posts");
+
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(morgan("common"));
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+mongoose.set("strictQuery", false)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(8800,() => {
+        console.log("Backend server is running")
+    })
+})
+.catch((error) =>{
+    console.log(error) 
+})
 app.get("/", (req,res)=>{
     res.send("welcome to homepage")
-})
-app.listen(8800,() => {
-    console.log("Backend server is running")
 })
