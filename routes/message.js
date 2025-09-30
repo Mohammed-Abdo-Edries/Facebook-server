3a677f315769df84487998cb21d85f4249dee319
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2025-09-30 08:20:52 +0200

    message rename back

--- routes/message.js
+++ routes/message.js
@@ -1,5 +1,5 @@
 import Conversation from "../models/Conversation.js";
-import Message from "../models/MessageTmp.js";
+import Message from "../models/Message.js";
 import express from "express";
 import {getReceiverSocketId,io} from "../socket.js";
 import protectRoute from "../middleware/protectRoute.js";
