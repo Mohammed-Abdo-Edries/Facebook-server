0b5f76889f1b0a1fa0308aeefbbca7700f4a4a75
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2025-09-30 10:32:51 +0200

    added console.log

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,7 @@ router.post("/register", async (req,res) => {
 });
 router.post("/login", async(req,res) =>{
     const { email, password } = req.body
+    console.log(email,password);
     try{
         const user = await User.login(email, password)
         const token = createToken(user._id)
